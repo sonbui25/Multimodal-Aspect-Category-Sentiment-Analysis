a358c0ef2c683990aa012cab1e27d65196d9c008
sonbui25 <thaisoncbq@gmail.com>
2026-02-01 11:29:00 +0700

    Update module fcmf

--- run_multimodal_fcmf.py
+++ run_multimodal_fcmf.py
@@ -581,7 +581,7 @@ def main():
             if isinstance(model, (DDP, torch.nn.DataParallel)):
                 model.module.load_state_dict(checkpoint['model_state_dict'])
             else:
-                model.load_state_dict(checkpoint['model_state_dict'], strict=False)
+                model.load_state_dict(checkpoint['model_state_dict'])
             
             # Load ResNets
             rimg_path = best_path.replace("fcmf", "resimg")
